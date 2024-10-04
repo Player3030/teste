e2bdc3259e0531db010aeefd8a7038a85a8dd83e
Player3030 <coffelek54@gmail.com>
2024-10-03 23:45:15 -0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,5 +1,5 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/Player3030/teste/blob/main/Example.lua"))()
-local SaveManager = loadstring(game:HttpGet(""))()
+local SaveManager = loadstring(game:HttpGet("https://github.com/Player3030/teste/blob/main/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Player3030/teste/refs/heads/main/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
